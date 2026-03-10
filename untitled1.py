09b346f2043091625cc901c1931be1edfff3a630
Metroacorn <ossi.kyhala@gmail.com>
2026-03-10 15:44:07 +0200

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -7,3 +7,13 @@ screen=pygame.display.set_mode(640,480)
 screen.fill(255,255,255)
 
 pygame.display.update
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    
+pygame.quit()
