470734cf9f575c89437a15a584fd4d26e605fbd8
Metroacorn <ossi.kyhala@gmail.com>
2026-03-12 15:30:34 +0200

    Update Bloons TDB.py

--- Bloons TDB.py
+++ Bloons TDB.py
@@ -9,7 +9,9 @@ screen.fill((255, 255, 255))
 start=pygame.Rect(0,0,50,50)
 end=pygame.Rect(0,0,50,50)
 start.center=(25,100)
-end.center=(400,455)
+end.center=(550,575)
+
+shop=pygame.Rect(600,0,400,600)
 
 pygame.display.update()
 
@@ -20,7 +22,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     pygame.draw.rect(screen,(255,0,0),start)
-    pygame.draw.rect(screen,(255,0,0),end)     
+    pygame.draw.rect(screen,(255,0,0),end) 
+    pygame.draw.rect(screen,(255,0,0),shop)    
     pygame.display.update()
-print("nananananananlmaofag")
+
 pygame.quit()
