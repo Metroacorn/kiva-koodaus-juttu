9121e39b1e3dd1227184d5dbea98ba857a34fa17
Metroacorn <ossi.kyhala@gmail.com>
2026-03-12 15:22:27 +0200

    hehehehe

---
+++ Bloons TDB.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000,600))
+
+screen.fill((255, 255, 255))
+
+start=pygame.Rect(0,0,50,50)
+end=pygame.Rect(0,0,50,50)
+start.center=(25,100)
+end.center=(400,455)
+
+pygame.display.update()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.draw.rect(screen,(255,0,0),start)
+    pygame.draw.rect(screen,(255,0,0),end)     
+    pygame.display.update()
+
+pygame.quit()
