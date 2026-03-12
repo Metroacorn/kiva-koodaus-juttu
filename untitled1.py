baa5589dde2d057a13350ef2ecc2b93ae8e0f280
Kirby-bite <johannesseppala@gmail.com>
2026-03-12 14:42:32 +0200

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -2,11 +2,11 @@ import pygame
 
 pygame.init()
 
-screen=pygame.display.set_mode(640,480)
+screen = pygame.display.set_mode((640, 480))
 
-screen.fill(255,255,255)
+screen.fill((255, 255, 255))
 
-pygame.display.update
+pygame.display.update()
 
 running = True
 
@@ -14,7 +14,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w]:
-        pygame.quit()
+        running = False
+
+pygame.quit()
