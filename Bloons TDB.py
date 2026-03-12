6445a5e24f8f781096a63d94bd211ae52b9afdfc
Metroacorn <ossi.kyhala@gmail.com>
2026-03-12 15:45:55 +0200

    Update Bloons TDB.py

--- Bloons TDB.py
+++ Bloons TDB.py
@@ -6,12 +6,20 @@ screen = pygame.display.set_mode((1000,600))
 
 screen.fill((255, 255, 255))
 
+dart_monkeyshop=pygame.Rect(0,0,50,50)
+tack_shootershop=pygame.Rect(0,0,50,50)
+boomerang_monkeyshop=pygame.Rect(0,0,50,50)
+
+dart_monkeyshop.center=(850,125)
+tack_shootershop.center=(850,225)
+boomerang_monkeyshop.center=(850,325)
+
 start=pygame.Rect(0,0,50,50)
 end=pygame.Rect(0,0,50,50)
 start.center=(25,100)
 end.center=(625,575)
 
-shop=pygame.Rect(700,0,400,600)
+shop=pygame.Rect(700,0,300,600)
 
 pygame.display.update()
 
@@ -23,7 +31,10 @@ while running:
             running = False
     pygame.draw.rect(screen,(255,0,0),start)
     pygame.draw.rect(screen,(255,0,0),end) 
-    pygame.draw.rect(screen,(255,0,0),shop)    
+    pygame.draw.rect(screen,(139, 69, 19),shop)    
+    pygame.draw.rect(screen,(0,255,0),dart_monkeyshop)
+    pygame.draw.rect(screen,(0,255,0),tack_shootershop)
+    pygame.draw.rect(screen,(0,255,0),boomerang_monkeyshop)
     pygame.display.update()
 
 pygame.quit()
